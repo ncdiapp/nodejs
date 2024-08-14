bf93c0bcf10448336bb5b1643ba66ad7df943b32
Xianghao Hu <xianghao_hu@hotmail.com>
2024-08-14 12:28:19 -0400

    homepage

--- express_api_nextjs/client/src/app/page.tsx
+++ express_api_nextjs/client/src/app/page.tsx
@@ -2,20 +2,20 @@
 'use client';
 //import { useAppContext } from "../contexts/AppContext";
 import { KioskCard } from "../components/KioskCard";
-import { KioskBottom } from "../components/KioskBottom";
-
 import { KioskIconBenefitsAndServices1 } from "../icons/KioskIconBenefitsAndServices1";
 import { KioskIconCemeteryInformation4 } from "../icons/KioskIconCemeteryInformation4";
-import { KioskIconGraveLocator1 } from "../icons/KioskIconGraveLocator1";
 import { KioskIconIntermentSchedule1 } from "../icons/KioskIconIntermentSchedule1";
+
+import { KioskIconGraveLocator1 } from "../icons/KioskIconGraveLocator1";
+import { KioskIconPhone } from "../icons/KioskIconPhone";
 
 import { useEffect, useState } from 'react';
 import Link from "next/link";
 
 const Home = () => {
   //const { cemeteryInfo } = useAppContext();
-  const [scaleValue, setScaleValue] = useState(1);
-  const [containerWidth, setContainerWidth] = useState(1920);
+  // const [scaleValue, setScaleValue] = useState(1);
+  // const [containerWidth, setContainerWidth] = useState(1920);
 
   const CEMETERY_ID = process.env.NEXT_PUBLIC_CEMETERY_ID
   const [cemeteryInfo, setCemeteryInfo] = useState<any>([]);
@@ -27,160 +27,154 @@ const Home = () => {
     } catch (error) {
       console.error(error);
     }
-  }; 
+  };
 
   useEffect(() => {
-    // Function to calculate the scale value based on screen height
-    const calculateScaleValue = () => {
-      fetchCemeteryInfo();
-      const screenHeight = window.innerHeight;
-      const originalHeight = 1600; // The original height of the container
-      const scale = screenHeight / originalHeight;
-      setContainerWidth(1920 * scale);
-      setScaleValue(scale);
-      
-    };
+    fetchCemeteryInfo();
 
-    // Calculate scale value on component mount
-    calculateScaleValue();
+    // const calculateScaleValue = () => {     
+    // const screenHeight = window.innerHeight;
+    // const originalHeight = 1600; // The original height of the container
+    // const scale = screenHeight / originalHeight;
+    // setContainerWidth(1920 * scale);
+    // setScaleValue(scale);
 
-    // Recalculate scale value on window resize
-    window.addEventListener('resize', calculateScaleValue);
+    // };
 
-    // Cleanup event listener on component unmount
-    return () => window.removeEventListener('resize', calculateScaleValue);
+    // // Calculate scale value on component mount
+    // calculateScaleValue();
+
+    // // Recalculate scale value on window resize
+    // window.addEventListener('resize', calculateScaleValue);
+
+    // // Cleanup event listener on component unmount
+    // return () => window.removeEventListener('resize', calculateScaleValue);
   }, []);
 
 
   return (
-    <div className="bg-[#f0f3f5] h-screen mx-auto" style={{ width: `${containerWidth}px` }}>
-      <div className="bg-grey-100 w-[1920px] h-[1600px]" style={{ transform: `scale(${scaleValue})`, 'transformOrigin': 'top left' }}>
-
-        <div className="w-full h-full absolute top-0 left-0 flex flex-col items-start">
-          <div className={`flex w-full h-[152px] items-center p-14 relative rotate-180 !self-stretch !h-64 !w-full`}>
-            <p
-              className={`relative w-fit mt-[-1.00px] rotate-180 font-KIOSK-h3 font-[number:var(--KIOSK-h3-font-weight)] text-transparent text-[length:var(--KIOSK-h3-font-size)] text-right tracking-[var(--KIOSK-h3-letter-spacing)] leading-[var(--KIOSK-h3-line-height)] whitespace-nowrap [font-style:var(--KIOSK-h3-font-style)] !mt-[unset] !opacity-0`}
-            >
-              <span className="text-[#212121] font-KIOSK-h3 [font-style:var(--KIOSK-h3-font-style)] font-[number:var(--KIOSK-h3-font-weight)] tracking-[var(--KIOSK-h3-letter-spacing)] leading-[var(--KIOSK-h3-line-height)] text-[length:var(--KIOSK-h3-font-size)]">
-                {cemeteryInfo?.name}
-              </span>
-              <span className="text-[#5b616b] font-KIOSK-h3 [font-style:var(--KIOSK-h3-font-style)] font-[number:var(--KIOSK-h3-font-weight)] tracking-[var(--KIOSK-h3-letter-spacing)] leading-[var(--KIOSK-h3-line-height)] text-[length:var(--KIOSK-h3-font-size)]">
-                {" "}
-                •{" "}
-              </span>
-              <span className="text-[#212121] font-KIOSK-h3 [font-style:var(--KIOSK-h3-font-style)] font-[number:var(--KIOSK-h3-font-weight)] tracking-[var(--KIOSK-h3-letter-spacing)] leading-[var(--KIOSK-h3-line-height)] text-[length:var(--KIOSK-h3-font-size)]">
-                VA/NCA
-              </span>
-            </p>
-          </div>
 
 
+    <div className="w-screen h-screen flex flex-col">
 
-          <div className="relative flex-1 self-stretch w-full grow bg-white flex flex-col items-start">
-            <div className="relative self-stretch w-full h-px" />
-            <div className="flex flex-col items-start relative flex-1 self-stretch w-full grow">
-              <div className="flex flex-col items-center justify-end gap-14 relative self-stretch w-full flex-[0_0_auto]">
-                <div className="flex flex-col items-center relative self-stretch w-full flex-[0_0_auto]">
-                  <div className="relative self-stretch w-full h-[200px] bg-[#ffffff]" />
-                  <div className="flex flex-col items-start gap-14 px-0 py-28 relative self-stretch w-full flex-[0_0_auto]">
-                    <div className="flex flex-col items-start gap-6 relative self-stretch w-full flex-[0_0_auto]">
-                      <div className="py-4 relative self-stretch mt-[-1.00px] font-KIOSK-h1 font-[number:var(--KIOSK-h1-font-weight)] text-kiosk-text-secondary text-center tracking-[var(--KIOSK-h1-letter-spacing)] leading-[var(--KIOSK-h1-line-height)] [font-style:var(--KIOSK-h1-font-style)] text-[44px]">
-                        Welcome to the
-                      </div>
-                      <div className="relative self-stretch [font-family:'Source_Sans_Pro',Helvetica] font-bold text-kioskbrand-primarydarker text-[64px] text-center tracking-[0] leading-[80px]">
-                        {cemeteryInfo?.name}
-                      </div>
-                      <p className="py-4 relative self-stretch font-KIOSK-h2 font-[number:var(--KIOSK-h2-font-weight)] text-kiosk-text-secondary text-center tracking-[var(--KIOSK-h2-letter-spacing)] leading-[var(--KIOSK-h2-line-height)] [font-style:var(--KIOSK-h2-font-style)] text-[32px]">
-                        Tap on a card to get started
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full">
-                <div className="h-0.5 bg-grey-200 relative self-stretch w-full" />
-                <div className="flex items-start relative flex-1 self-stretch w-full grow bg-white">
-                  <Link href="/gravelocator">
-                    <KioskCard
-                      KIOSKCardIconIcon={
-                        <KioskIconGraveLocator1 className="!absolute !w-40 !h-40 !top-5 !left-5 !object-cover" />
-                      }
-                      cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset] !border-[unset]"
-                      className="!self-stretch !h-[unset] !flex-1 !shadow-[unset] !grow !w-[unset]"
-                      contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
-                      state="default"
-                      text={
-                        <>
-                          Grave <br />
-                          Locator
-                        </>
-                      }
-                    />
-                  </Link>
 
-                  <div className="relative self-stretch w-0.5 bg-grey-200" />
-                  <Link href="/cemeteryinfo">
-                    <KioskCard
-                      KIOSKCardIconIcon={
-                        <KioskIconCemeteryInformation4 className="!absolute !w-40 !h-40 !top-5 !left-5 !object-cover" />
-                      }
-                      cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset] !border-[unset]"
-                      className="!self-stretch !h-[unset] !flex-1 !shadow-[unset] !grow !w-[unset]"
-                      contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
-                      state="default"
-                      text={
-                        <>
-                          Cemetery <br />
-                          Information
-                        </>
-                      }
-                    />
-                  </Link>
-                  <div className="relative self-stretch w-0.5 bg-grey-200" />
-                  <KioskCard
-                    KIOSKCardIconIcon={
-                      <KioskIconBenefitsAndServices1 className="!absolute !w-40 !h-40 !top-5 !left-5 !object-cover" />
-                    }
-                    cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset]"
-                    className="!self-stretch !h-[unset] !flex-1 !shadow-[unset] !grow !w-[unset]"
-                    contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
-                    state="default"
-                    text={
-                      <>
-                        Benefits <br />
-                        and Services
-                      </>
-                    }
-                  />
-                  <div className="relative self-stretch w-0.5 bg-grey-200" />
-                  <KioskCard
-                    KIOSKCardIconIcon={
-                      <KioskIconIntermentSchedule1 className="!absolute !w-40 !h-40 !top-5 !left-5 !object-cover" />
-                    }
-                    cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset] !border-[unset]"
-                    className="!self-stretch !h-[unset] !flex-1 !shadow-[unset] !grow !w-[unset]"
-                    contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
-                    state="default"
-                    text={
-                      <>
-                        Interment <br />
-                        Schedule
-                      </>
-                    }
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="h-2 bg-kiosk-gold relative self-stretch w-full" />
-          </div>
-          <KioskBottom className="!self-stretch !flex-[0_0_auto] !w-full" page="home-page" />
+      <div className="w-full h-[260px] relative">
+        <div className="w-full h-[50%] bg-grey-100 "></div>
+        <div className="flex w-full h-[50%] bg-white">
         </div>
-        <div className="absolute w-[400px] h-[400px] top-14 left-[760px] bg-grey-100 rounded-[200px]" style={{ "left": "calc(50% - 200px)" }}>
-          <img className="absolute w-[352px] h-[352px] top-6 left-6" alt="Logo" src="img/logo.png" />
+        <div className="absolute w-[215px] h-[215px] inset-0 m-auto bg-grey-100 rounded-full p-3">
+          <img className="w-full h-full" alt="Logo" src="img/logo.png" />
+        </div>
+      </div>
+
+      <div className="w-full flex-auto">
+        <div className="w-full text-center h-[50%]">
+          <div className="py-4 font-KIOSK-h1 font-bold text-kiosk-text-secondary text-center tracking-[var(--KIOSK-h1-letter-spacing)] [font-style:var(--KIOSK-h1-font-style)] text-[24px] leading-8">
+            Welcome to the
+          </div>
+          <div className="[font-family:'Source_Sans_Pro',Helvetica] font-bold text-kioskbrand-primarydarker text-[32px] leading-8">
+            {cemeteryInfo?.name}
+          </div>
+          <p className="py-4 font-KIOSK-h2 font-bold text-kiosk-text-secondary text-center tracking-[var(--KIOSK-h2-letter-spacing)] leading-[var(--KIOSK-h2-line-height)] [font-style:var(--KIOSK-h2-font-style)] text-[20px] leading-8">
+            Tap on a card to get started
+          </p>
+        </div>
+
+        <div className="w-full h-[50%] overflow-hidden">
+          <div className="h-0.5 bg-grey-200 relative self-stretch w-full" />
+          <div className="flex w-full bg-white">
+            <Link className="h-full w-full flex-auto" href="/gravelocator">
+              <KioskCard
+                KIOSKCardIconIcon={
+                  <KioskIconGraveLocator1 className="!absolute !w-20 !h-20 !top-5 !left-5 !object-cover" />
+                }
+                cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset] !border-[unset]"
+                className="w-full h-full"
+                contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
+                state="default"
+                text={
+                  <>
+                    Grave <br />
+                    Locator
+                  </>
+                }
+              />
+            </Link>
+
+            <div className="relative self-stretch w-0.5 bg-grey-200" />
+            <Link className="h-full w-full flex-auto" href="/cemeteryinfo">
+              <KioskCard
+                KIOSKCardIconIcon={
+                  <KioskIconCemeteryInformation4 className="!absolute !w-20 !h-20 !top-5 !left-5 !object-cover" />
+                }
+                cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset] !border-[unset]"
+                className="w-full h-full"
+                contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
+                state="default"
+                text={
+                  <>
+                    Cemetery <br />
+                    Information
+                  </>
+                }
+              />
+            </Link>
+            <div className="relative self-stretch w-0.5 bg-grey-200" />
+            <KioskCard
+              KIOSKCardIconIcon={
+                <KioskIconBenefitsAndServices1 className="!absolute !w-20 !h-20 !top-5 !left-5 !object-cover" />
+              }
+              cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset]"
+              className="h-full w-full flex-auto"
+              contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
+              state="default"
+              text={
+                <>
+                  Benefits <br />
+                  and Services
+                </>
+              }
+            />
+            <div className="relative self-stretch w-0.5 bg-grey-200" />
+            <KioskCard
+              KIOSKCardIconIcon={
+                <KioskIconIntermentSchedule1 className="!absolute !w-20 !h-20 !top-5 !left-5 !object-cover" />
+              }
+              cardClassName="!rounded-[unset] !border-[unset] ![border-image:unset] !border-[unset]"
+              className="h-full w-full flex-auto"
+              contentClassName="!mr-[-20.75px] !ml-[-20.75px]"
+              state="default"
+              text={
+                <>
+                  Interment <br />
+                  Schedule
+                </>
+              }
+            />
+          </div>
         </div>
 
       </div>
+      <div className="relative self-stretch w-full h-2 bg-kiosk-gold" />
+      <div className="w-full h-[80px] flex items-center bg-kioskbrand-primarydarker gap-3 justify-center">
+        <KioskIconPhone className="!relative !w-5 !h-5" color="white" />
+        <p className="relative w-fit mt-[-1.00px] [font-family:'Source_Sans_Pro',Helvetica] font-normal text-white text-[18px] text-center">
+          <span className="leading-[var(--KIOSK-body-line-height)] font-KIOSK-body [font-style:var(--KIOSK-body-font-style)] font-[number:var(--KIOSK-body-font-weight)] tracking-[var(--KIOSK-body-letter-spacing)] text-[14px]">
+            Are you eligible for VA Survivors benefits? To find out contact the VA at{" "}
+          </span>
+          <span className="font-[number:var(--KIOSK-h3-font-weight)] leading-[var(--KIOSK-h3-line-height)] font-KIOSK-h3 [font-style:var(--KIOSK-h3-font-style)] tracking-[var(--KIOSK-h3-letter-spacing)] text-[12px]">
+            1-800-827-1000
+          </span>
+        </p>
+        <div className="relative w-5 h-5" />
+      </div>
     </div>
+
+
+
+
+
+
   );
 };
 export default Home;
--- express_api_nextjs/client/src/components/BaseKioskIcon.tsx
+++ express_api_nextjs/client/src/components/BaseKioskIcon.tsx
@@ -11,10 +11,10 @@ interface Props {
 
 export const BaseKioskIcon = ({ className }: Props): JSX.Element => {
   return (
-    <div className={`w-[200px] h-[200px] bg-grey-50 rounded-2xl overflow-hidden ${className}`}>
-      <div className="relative w-[204px] h-[204px] -top-0.5 -left-0.5 rounded-[0px_0px_240px_0px] border-2 border-solid border-white [background:linear-gradient(180deg,rgb(248,249,250)_0%,rgb(233,236,239)_100%)]">
-        <div className="w-[104px] h-[104px] rounded-[0px_0px_120px_0px] absolute -top-0.5 -left-0.5 border-2 border-solid border-white" />
-        <div className="w-[154px] h-[154px] rounded-[0px_0px_180px_0px] absolute -top-0.5 -left-0.5 border-2 border-solid border-white" />
+    <div className={`w-[100px] h-[100px] bg-grey-50 rounded-2xl overflow-hidden ${className}`}>
+      <div className="relative w-[102px] h-[102px] -top-0.5 -left-0.5 rounded-[0px_0px_120px_0px] border-2 border-solid border-white [background:linear-gradient(180deg,rgb(248,249,250)_0%,rgb(233,236,239)_100%)]">
+        <div className="w-[104px] h-[104px] rounded-[0px_0px_60px_0px] absolute -top-0.5 -left-0.5 border-2 border-solid border-white" />
+        <div className="w-[154px] h-[154px] rounded-[0px_0px_90px_0px] absolute -top-0.5 -left-0.5 border-2 border-solid border-white" />
       </div>
     </div>
   );
